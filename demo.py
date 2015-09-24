b04fc26c9bed230af4992fcb0be0fd1c9db9e46b
huangxianxin <315655076@qq.com>
2015-09-24 15:12:56 +0800

    a simple model to control command

---
+++ demo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python     
+import time
+import os
+import datetime
+import click
+
+@click.command()
+@click.option()
+
+commandOne = "./update-elastic -H es.cluster.ldp:19200 -i sevenga.ironcommander@"
+initTime = datetime.date(2015,7,20)
+while True:  
+    initTime = initTime + datetime.timedelta(days=1)
+    strInitTime = initTime.strftime('%Y-%m-%d')
+    command = commandOne + strInitTime + " --product"
+    print(command)
+    os.system(command)
+    if strInitTime == '2015-08-24':
+        break
+    time.sleep(1800)
+    
+
